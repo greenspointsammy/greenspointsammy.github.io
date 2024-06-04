76d0647f45b410a68ec62b520bca6a68e646fa8b
S. Yanez <156263950+greenspointsammy@users.noreply.github.com>
2024-06-04 00:16:31 -0400

    Update time.js

--- time.js
+++ time.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+ReactDOM.render(
+  <button>Beat me up Sammy</button>
+document.getElementById("panel")
+);
+<div id="panel">index.html</div>
 fetch('path/to/your/json/file.json')
   .then(response => response.json())
   .then(data => {
